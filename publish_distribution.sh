77a380c52ce78855ffa131446a76dc958c32c892
Brian O'Neill <bone@alumni.brown.edu>
2016-11-10 16:13:29 -0500

    seriously typo?

--- publish_distribution.sh
+++ publish_distribution.sh
@@ -1,1 +1,4 @@
-aws s3 cp extras/kinesis-asl-assembly/target/spark-streaming-kinesis-asl-assembly_2.10-1.6.1.jar s3://artifact-monetate-dev/gradle/spark-streaming-kinesis-asl-assembly_2.10-1.6.1.monetate2.jar
+#aws s3 cp extras/kinesis-asl-assembly/target/spark-streaming-kinesis-asl-assembly_2.10-1.6.3.jar \
+#         s3://artifact-monetate-dev/gradle/spark-streaming-kinesis-asl-assembly_2.10-1.6.3.monetate3.jar
+aws s3 cp extras/kinesis-asl-assembly/target/spark-streaming-kinesis-asl-assembly_2.11-1.6.3.jar \
+          s3://artifact-monetate-dev/gradle/spark-streaming-kinesis-asl-assembly_2.11-1.6.3.monetate3.jar
